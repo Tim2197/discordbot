082f8bbfe5d1b48a898a63acdb3017024eec2b2b
Tim2197 <79378803+Tim2197@users.noreply.github.com>
2022-06-05 13:05:21 -0500

    Added mongoose package to handle mongodb <-> application

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, './.env') })
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 const { Client, Intents } = require('discord.js');
+const mongoose = require('mongoose');
 const token = process.env.TOKEN;
 const password = process.env.PASS;
 const username = process.env.USER;
+const User = require('./models/user')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+//const { MongoClient, ServerApiVersion } = require('mongodb');
+//const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const uri = `mongodb+srv://${username}:${password}@cluster0.q4cnn.mongodb.net/?retryWrites=true&w=majority`;
-const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => console.log('connected to database'))
+  .catch((err) => console.log(err));
 
 
 client.on('message', msg => {
