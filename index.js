0e80f1e188c905039ddcb2293d629ded486257da
Tim2197 <79378803+Tim2197@users.noreply.github.com>
2022-06-05 12:03:08 -0500

    Added MongoDB integration, and prettier to node packages.

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 
 const { Client, Intents } = require('discord.js');
 const token = process.env.TOKEN;
+const password = process.env.PASS;
+const username = process.env.USER;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${username}:${password}@cluster0.q4cnn.mongodb.net/?retryWrites=true&w=majority`;
+const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 client.on('message', msg => {
-    if (msg.content === "you a bitch") {
-        msg.message('no u');
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
     }
-});
+  });
 
 client.on('ready', () => {
     console.log(`logged in as ${client.user.tag}!`)
